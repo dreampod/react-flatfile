fa029138fd746c9f8713b002c6d2e9de64831b47
Kevin Yu <kevinyu@dreampod.com>
2020-08-01 12:05:28 -0500

    working demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlatfileImporter from "flatfile-csv-importer";
+
+const LICENSE_KEY = '88710974-8ddc-4c56-a7e7-a051c4e7045f'
+
 
 function App() {
+  const importer = new FlatfileImporter(LICENSE_KEY, {
+    fields: [{
+      label: 'Product ID',
+      key: 'product-id',
+      isRequired: true
+    }, {
+      label: 'Name',
+      key: 'product-name',
+      isRequired: true
+    }, {
+      label: 'Price',
+      key: 'price',
+      isRequired: true
+    }, {
+      label: 'Category',
+      key: 'category',
+      alternates: ['type', 'type id'],
+      isRequired: false
+    }],
+    type: 'Product',
+    allowCustom: false,
+    maxRecords: 100
+  })
+  const launch = () => {
+    importer.requestDataFromUser()
+    .then(results => {
+      // Tell us what you want to happen to this data
+    })
+    .catch(function(error) {
+      console.info(error || "window close");
+    });
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+            type="button"
+            id="launch"
+            value="Launch Importer"
+            onClick={launch}
+            />
       </header>
     </div>
   );
